26b716fc78e7c8e7d08a708f2d5fe1a1d8874d0a
Nasanin-Parast <nasi.parast@gmail.com>
2023-02-20 15:48:05 +0100

    add Charakter,Dieb,Kaempfer,Magie and Monster classes

---
+++ src/main/java/Dieb.java
@@ -0,0 +1,5 @@
+public class Dieb extends Charakter {
+    public Dieb(String name, int intelligenz, int staerke, int konstitution, int weisheit, int geschicklichkeit) {
+        super(name, intelligenz, staerke, konstitution, weisheit, geschicklichkeit, 8);
+    }
+}
---
+++ src/main/java/Kaempfer.java
@@ -0,0 +1,5 @@
+public class Kaempfer extends Charakter{
+    public Kaempfer(String name, int intelligenz, int staerke, int konstitution, int weisheit, int geschicklichkeit) {
+        super(name, intelligenz, staerke, konstitution, weisheit, geschicklichkeit, 10);
+    }
+}
