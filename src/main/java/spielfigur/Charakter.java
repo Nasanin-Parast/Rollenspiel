59effd3f99e41d91cfcce5d15df3ddb4ed2ff206
moodyRudi <r.gardemann@web.de>
2023-02-23 11:07:45 +0100

    changing package for character classes

--- src/main/java/spielfigur/Charakter.java
+++ src/main/java/spielfigur/Charakter.java
@@ -4,5 +4,4 @@ public abstract class Charakter extends Spielfigur {
     public Charakter(String name, int intelligenz, int staerke, int konstitution, int weisheit, int geschicklichkeit, int basisLP) {
         super(name, intelligenz, staerke, konstitution, weisheit, geschicklichkeit, basisLP);
     }
-
 }
--- src/main/java/spielfigur/Kaempfer.java
+++ src/main/java/spielfigur/Kaempfer.java
@@ -1,7 +1,5 @@
 package spielfigur;
 
-import spielfigur.Charakter;
-
 public class Kaempfer extends Charakter {
     public Kaempfer(String name, int intelligenz, int staerke, int konstitution, int weisheit, int geschicklichkeit) {
         super(name, intelligenz, staerke, konstitution, weisheit, geschicklichkeit, 10);
