2716020a503c5aecceb5166f1a37052db837ba47
moodyRudi <r.gardemann@web.de>
2023-03-04 14:15:39 +0100

    work on game board

--- src/main/java/oberflaeche/controller/ControllerImpl.java
+++ src/main/java/oberflaeche/controller/ControllerImpl.java
@@ -6,26 +6,6 @@ import oberflaeche.model.Fenster;
 public class ControllerImpl {
     private Fenster fenster = new Fenster();
 
-    public boolean isReadyToSearch() {
-        return fenster.isReadyToSearch();
-    }
-
-    public void wandPlatzieren(int r, int c) {
-        fenster.wandPlatzieren(r, c);
-    }
-
-    public void wandLoeschen(int r, int c) {
-        fenster.wandLoeschen(r, c);
-    }
-
-    public void generateEdges() {
-        fenster.generateEdges();
-    }
-
-    public void clearEdges() {
-        fenster.clearEdges();
-    }
-
     public void clear() {
         fenster.clear();
     }
@@ -46,21 +26,5 @@ public class ControllerImpl {
 
     public int getColumnSize() {
         return fenster.getColumnSize();
-    }
-
-    public int getSourceXCordinate() {
-        return fenster.getSourceXCordinate();
-    }
-
-    public int getSourceYCordinate() {
-        return fenster.getSourceYCordinate();
-    }
-
-    public int getSinkXCordinate() {
-        return fenster.getSinkXCordinate();
-    }
-
-    public int getSinkYCordinate() {
-        return fenster.getSinkYCordinate();
     }
 }
--- src/main/java/oberflaeche/model/Knoten.java
+++ src/main/java/oberflaeche/model/Knoten.java
@@ -4,25 +4,9 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Knoten implements Comparable<Knoten> {
-    ArrayList<Knoten> edges = new ArrayList<Knoten>();
+    ArrayList<Knoten> edges = new ArrayList<>();
     int row = -1;
     int estimatedDistance = 0;
-
-    public int getRow() {
-        return row;
-    }
-
-    public void setRow(int row) {
-        this.row = row;
-    }
-
-    public int getColumn() {
-        return column;
-    }
-
-    public void setColumn(int column) {
-        this.column = column;
-    }
 
     int column = -1;
     boolean isRock = false;
@@ -31,27 +15,8 @@ public class Knoten implements Comparable<Knoten> {
         column = c;
     }
 
-    public Knoten(int r, int c, boolean rock) {
-        row = r;
-        column = c;
-        isRock = rock;
-    }
-
     public void addEdge(Knoten edge) {
         edges.add(edge);
-    }
-
-
-    public void removeEdge(Knoten edge) {
-        edges.remove(edge);
-    }
-
-    public void clearEdges() {
-        edges.clear();
-    }
-
-    public Iterator<Knoten> keySet() {
-        return edges.iterator();
     }
 
     @Override
